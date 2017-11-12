d0ae00cf0f5f675a03a0210eb6b65c5744e8b32d
Avi Flombaum <avi@flombaum.com>
2017-11-12 23:30:24 +0000

    Done.

--- question.js
+++ question.js
@@ -1,1 +1,25 @@
 'use strict';
+
+class Question{
+  constructor(content){
+    this.content = content
+    this.save()
+  }
+
+  save(){
+    this.constructor._All.push(this)
+  }
+
+  static All(){
+    return this._All;
+  }
+
+  static Find(id){
+    // id 1
+    // [Question1, Question2, Question3]
+    // index 0 id - 1
+    return this.All()[id - 1]
+  }
+}
+
+Question._All = []
